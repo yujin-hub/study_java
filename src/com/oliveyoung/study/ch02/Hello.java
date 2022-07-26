977efc85610180d19dd2765fc21e12a38ba7bb81
cuj0405@naver.com <cuj0405@naver.com>
2022-07-26 16:14:56 +0900

    .

---
+++ src/com/oliveyoung/study/ch02/Hello.java
@@ -0,0 +1,54 @@
+package com.oliveyoung.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 데이터타입 변수명 = 초기값;
+		// 데이터타입 변수명;
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+		int width = 100;
+		String name = "최유진";
+		
+		
+		// 올리브영
+		String brand = "클리오";
+		String item = "클리오 프로아이팔레트 AD";
+		int price = 22400;
+		String shipment ="일반배송";
+		String option = "14호 한남동 아뜰리에";
+		int num = 2;
+		int total = 44800;
+		double starscore= 4.8;
+		String nickname = "레몬사탕";
+		String grade = "탑리뷰어";
+		String review = "누드, 가을뮤트 계열 좋아하는 분들 ,,";		
+		String question = "언제쯤 배송되나요";
+		String answer = "클리오 담당자입니다.";
+		String info = "용량 또는 중량 ,,";
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은 : " + name);
+		
+		System.out.println("브랜드명 - " + brand);
+		System.out.println("제품명 - " + item);
+		System.out.println("가격 - " + price);
+		System.out.println("배송 형태 - " + shipment);
+		System.out.println("내가 살 팔레트 호수 - " + option);
+		System.out.println("수량 - " + num +"개");
+		System.out.println("총 금액은 " + total + " 입니다.");
+		System.out.println("평점 - " + starscore);
+		System.out.println("리뷰어 이름 - " + nickname);
+		System.out.println("리뷰어 등급 - " + grade);
+		System.out.println("솔직 리뷰 -  " + review);
+		System.out.println("질문 - " + question);
+		System.out.println("답 - " + answer);
+		System.out.println("구매정보 - " + info);
+
+	}
+}
