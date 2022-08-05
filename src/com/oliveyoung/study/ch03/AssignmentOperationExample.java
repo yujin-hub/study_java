014187a2b8123c7dc67bf2f25d83daa2772b03c4
cuj0405@naver.com <cuj0405@naver.com>
2022-08-05 11:21:53 +0900

    .

---
+++ src/com/oliveyoung/study/ch03/AssignmentOperationExample.java
@@ -0,0 +1,32 @@
+package com.oliveyoung.study.ch03;
+
+public class AssignmentOperationExample {
+
+	public static void main(String[] args) {
+
+		// += 
+		int result = 0;
+		
+		result += 10;  
+		
+		System.out.println("result: "  + result);
+
+		
+		// -=
+		int result2 = 15;
+		
+		result2 -= 2;
+		
+		System.out.println("result2: " + result2);
+		
+		
+		// *=
+		int result3 = 2;
+		
+		result3 *= 3;
+		
+		System.out.println("result3: " + result3);
+		
+	}
+
+}
