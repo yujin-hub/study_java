855f124d5e01863895350f8473f8943d4461f661
cuj0405@naver.com <cuj0405@naver.com>
2022-08-17 17:12:39 +0900

    .

---
+++ src/com/oliveyoung/study/ch05/ArrayCreateByNewExample.java
@@ -0,0 +1,20 @@
+package com.oliveyoung.study.ch05;
+
+public class ArrayCreateByNewExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr1 = new int[3];
+		
+		int[] arr2;
+		arr2 = new int[5];
+	
+		arr1[0] = 77;
+		arr1[1] = 28;
+		arr1[2] = 89;
+		
+		System.out.println(arr1[2]);
+	}
+
+}
