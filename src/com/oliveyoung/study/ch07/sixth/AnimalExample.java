543fa2c924d9e96edc811088518b8c4eeb9334e7
cuj0405@naver.com <cuj0405@naver.com>
2022-08-29 17:18:01 +0900

    .

---
+++ src/com/oliveyoung/study/ch07/sixth/AnimalExample.java
@@ -0,0 +1,14 @@
+package com.oliveyoung.study.ch07.sixth;
+
+public class AnimalExample {
+
+	public static void main(String[] args) {
+
+		Cat cat = new Cat();
+		Dog dog = new Dog();
+		cat.sound();
+		System.out.println("-------");
+		dog.sound();
+	}
+
+}
